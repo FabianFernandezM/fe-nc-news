289f2197294b625ed5167509c52b18a6297ab51d
Fabian <xlyncex@gmail.com>
2024-05-07 11:05:05 +0100

    Modified placeholder text on articles

--- src/Modules/SmallCard.jsx
+++ src/Modules/SmallCard.jsx
@@ -4,7 +4,7 @@ export default function SmallCard({article}) {
         <div className="card-small">
             <img className="card-image" src={article.article_img_url} alt="Post image" />
             <h2 className="card-title">{article.title}</h2>
-            <p className="card-body">I regret to inform all living beings that dinos are extinct no more. There have been several attacks on cities such as Pochilandia and Rubbermypants over the last few days, with a total of 4 children being killed whilst adults celebrated their deaths with alcohol and drugs. </p>
+            <p className="card-body">"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
             <div className="card-icons">
                 <h1 className ="icon">^</h1>
                 <h1 className ="votes-counter">{article.comment_count}</h1>
