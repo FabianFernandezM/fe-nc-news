1f4289d5ef6ac45bc76342e779a6aee9a62a5a78
Fabian <xlyncex@gmail.com>
2024-05-08 12:18:14 +0100

    Completed post a comment functionality. Please note: as of now, it refreshes the page once comment has been posted. This may be revisited

--- src/Contexts/User.jsx
+++ src/Contexts/User.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react"
 export const UserContext = createContext()
 
 export const UserProvider = (({children})=>{
-    const [user, setUser] = useState('TestUser')
+    const [user, setUser] = useState('jessjelly')
 
     return <UserContext.Provider value={{user, setUser}}>{children}</UserContext.Provider>
 })
--- src/Modules/BigCard.jsx
+++ src/Modules/BigCard.jsx
@@ -1,26 +1,34 @@
 import CommentCard from "./CommentCard"
 import { useEffect, useState, useRef } from "react"
 import axios from 'axios';
+import CommentForm from "./CommentForm";
 
 
-export default function BigCard({article, comments}) {
+export default function BigCard({article, comments, setComments, updatePage, setUpdatePage}) {
 
     const [patchValue, setPatchValue] = useState(0)
     const [currVote, setCurrVote] = useState(0)
     const [loadingVotes, setLoadingVotes] = useState(false)
+    const [showCommentForm, setShowCommentForm] = useState(false)
 
     const handleVote = (value) => {
         setPatchValue(value)
         setCurrVote(currVote+value)
     }
 
+    const handleShowCommentForm = ()=> {
+        setShowCommentForm(!showCommentForm)
+    }
+
     useEffect(()=> {
         setLoadingVotes(true)
-        if (article.length !== 0) 
-        axios.patch(`https://nc-news-78g8.onrender.com/api/articles/${article.article_id}`, { inc_votes : patchValue })
-        .then((data)=>{
-            setLoadingVotes(false)
-        })
+        if (article.length !== 0) {
+            axios.patch(`https://nc-news-78g8.onrender.com/api/articles/${article.article_id}`, { inc_votes : patchValue })
+            .then((data)=>{
+                setLoadingVotes(false)
+            })
+        }
+        
     }, [currVote])
 
    return (
@@ -34,7 +42,9 @@ export default function BigCard({article, comments}) {
                 {loadingVotes ? <h2 className ="votes-counter">...</h2> : <h2 className ="votes-counter">{article.votes+currVote}</h2>}
                 <button className ="icon"  disabled={currVote===-1} onClick={() => handleVote(-1)}>v</button>
             </div>
+            <button className ="big-card-comment-button"  disabled={currVote===1} onClick={handleShowCommentForm}>{showCommentForm ? "Hide Form" : "Post a comment"}</button>
             <div className="card-comments">
+                {showCommentForm ? <CommentForm article_id={article.article_id}comments={comments} setComments={setComments} updatePage={updatePage} setUpdatePage={setUpdatePage}/> : null}
                 {comments.map((comment) => {
                     return <CommentCard key={comment.comment_id} comment={comment}/>
                 })}
