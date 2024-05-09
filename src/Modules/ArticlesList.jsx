37c04788541ba0c177e52d8ac41ba9fe779edaad
Fabian <xlyncex@gmail.com>
2024-05-09 16:37:36 +0100

    Fixed issue with URLqueries

--- src/Modules/ArticlesList.jsx
+++ src/Modules/ArticlesList.jsx
@@ -11,6 +11,7 @@ export default function ArticlesList({showSearch}) {
     const [topicQuery, setTopicQuery] = useState(searchParams.get("topic") || "");
     const [sortByQuery, setSortByQuery] = useState(searchParams.get("sort_by") || "");
     const [orderQuery, setOrderQuery] = useState(searchParams.get("order") || "");
+    const [pageQuery, setPageQuery] = useState(searchParams.get("p") || "");
     const navigate = useNavigate();
 
     const [articlesList, setArticlesList] = useState([])
@@ -32,15 +33,11 @@ export default function ArticlesList({showSearch}) {
 
 
     useEffect(()=>{
+
         let urlString = "https://nc-news-78g8.onrender.com/api/articles"
         let prevQuery = false;
 
         setIsLoading(true)
-
-        if(topicQuery.length === 0 && sortByQuery.length === 0 && orderQuery.length === 0) {
-            urlString = "https://nc-news-78g8.onrender.com/api/articles"
-            navigate(`/articles`)
-        }
 
         if (topicQuery.length !== 0) {
             if (!prevQuery) { urlString += `?`; prevQuery=true } else { urlString += `&` }
@@ -55,11 +52,12 @@ export default function ArticlesList({showSearch}) {
             urlString += `order=${orderQuery}`
         }
 
-
-
         axios.get(urlString)
-        .then(data => setArticlesList(data.data.articles))
-        .then(()=> setIsLoading(false))
+        .then(data => {setArticlesList(data.data.articles); console.log(data.data)})
+        .then(()=> {
+            setIsLoading(false);
+            navigate(urlString.slice(37))
+        })
         .catch((error)=>{
             setError({code: error.response.status, message: error.response.data.message})
         })
