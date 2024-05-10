ead16194da32d33f0d20ac05f3cbf103def2d10a
Fabian <xlyncex@gmail.com>
2024-05-10 15:50:39 +0100

    Added popup window for unavailable features

--- src/Modules/ErrorPage.jsx
+++ src/Modules/ErrorPage.jsx
@@ -1,7 +1,9 @@
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import "../App.css"
 
 export default function ErrorPage({error}) {
+
+    const navigate = useNavigate();
 
     return (
         <>
@@ -9,7 +11,7 @@ export default function ErrorPage({error}) {
                 <img src="../../resources/Images/oops_error.png" alt="Error Image" />
                 <h2>Something went terribly wrong...</h2>
                 {error ? <h3>{error.code} - {error.message}</h3> : <h3>404 - Not Found</h3>}
-                <Link to={`/`} className="icon"><h3>Back</h3></Link>
+                <Link to={"/articles"}><button className="icon icon-prev"><h2></h2></button></Link>
             </div>
         </>
     )
--- src/Modules/LoadingIcon.jsx
+++ src/Modules/LoadingIcon.jsx
@@ -3,7 +3,7 @@ export default function LoadingIcon () {
         <div className="loading-container">
             <div className="loading-icon loading-3"></div>
             <div className="loading-icon loading-2"></div>
-            <div className="loading-icon loading-1"><h2 class="loading-text">Loading...</h2></div>
+            <div className="loading-icon loading-1"><h2 className="loading-text">Loading...</h2></div>
         </div>
     )
 }
