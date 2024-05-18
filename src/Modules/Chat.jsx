c25ba43c25418255d2c27206e461968fc6001566
Fabian <xlyncex@gmail.com>
2024-05-18 17:15:50 +0100

    socket-chat working locally

---
+++ src/Modules/Chat.jsx
@@ -0,0 +1,37 @@
+
+import io from "socket.io-client"
+import {useEffect, useState} from "react"
+
+const socket = io.connect("http://localhost:3000")
+
+export default function Chat() {
+  const [room, setRoom] = useState("1")
+  const [message, setMessage] = useState("")
+  const [messageReceived, setMessageReceived] = useState("")
+
+  const joinRoom = () => {
+    if (room!== "") socket.emit("join_room", room)
+  }
+
+  const sendMessage = ()=> {
+    socket.emit("send_message", {message , room})
+  }
+
+  useEffect(()=>{
+    socket.on("receive_message", (data)=>{
+      setMessageReceived(data.message)
+    })
+  }, [socket])
+
+  joinRoom()
+
+  return (
+    <div className="App">
+      <input placeholder='Room...' onChange={(e) => {setRoom(e.target.value)}}></input>
+      <button onClick={joinRoom}>Join Room</button>
+      <input placeholder='Message...' onChange={(e) => {setMessage(e.target.value)}}></input>
+      <button onClick={sendMessage}>Send message</button>
+      <p>{messageReceived}</p>
+    </div>
+  );
+}
